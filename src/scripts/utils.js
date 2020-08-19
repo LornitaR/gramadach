6e25a2fcfd58b9058893b66ad94164af82999740
Lorna Ryan <lornaniriain@gmail.com>
2020-08-19 22:51:20 +0100

    update styling

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,12 +1,12 @@
 function dúnMenu() {
     document.body.style.gridTemplateAreas = '"header header" "main main" "footer footer"';
-    document.getElementById('dún').style.visibility = 'hidden';
-    document.getElementById('menu').style.display = 'block';
+    document.getElementById('dún').style.display = 'none';
+    document.getElementById('menu').style.display = '';
 }
 
 
 function taispeáinMenu() {
     document.body.style.gridTemplateAreas = '"header header" "sidebar sidebar" "footer footer"';
     document.getElementById('menu').style.display = 'none';
-    document.getElementById('dún').style.visibility = 'visible';
+    document.getElementById('dún').style.display = '';
   }
