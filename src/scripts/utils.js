087d4f9de09201683ac50ace4aff634fa434bc57
Lorna Ryan <lornaniriain@gmail.com>
2020-08-20 00:08:33 +0100

    fix issue with missing close menu button;

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,12 +1,13 @@
 function dúnMenu() {
     document.body.style.gridTemplateAreas = '"header header" "main main" "footer footer"';
     document.getElementById('dún').style.display = 'none';
-    document.getElementById('menu').style.display = '';
+    document.getElementById('menu').style.display = 'block';
 }
 
 
 function taispeáinMenu() {
     document.body.style.gridTemplateAreas = '"header header" "sidebar sidebar" "footer footer"';
     document.getElementById('menu').style.display = 'none';
-    document.getElementById('dún').style.display = '';
+    document.getElementById('dún').style.display = 'block';
+    document.getElementById('dún').style.visibility = 'visible ';
   }
