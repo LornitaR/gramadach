7a7ba3fc742a4202d18e89a01ca82994e92917dd
Lorna Ryan <lornaniriain@gmail.com>
2020-08-20 10:41:38 +0100

    fix display issues on desktop site

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,12 +1,16 @@
 function dúnMenu() {
-    document.body.style.gridTemplateAreas = '"header header" "main main" "footer footer"';
+    if(window.visualViewport.width <= 420) {
+        document.body.style.gridTemplateAreas = '"header header" "main main" "footer footer"';
+    }
     document.getElementById('dún').style.display = 'none';
     document.getElementById('menu').style.display = 'block';
 }
 
 
 function taispeáinMenu() {
-    document.body.style.gridTemplateAreas = '"header header" "sidebar sidebar" "footer footer"';
+    if(window.visualViewport.width <= 420) {
+        document.body.style.gridTemplateAreas = '"header header" "sidebar sidebar" "footer footer"';
+    }
     document.getElementById('menu').style.display = 'none';
     document.getElementById('dún').style.display = 'block';
     document.getElementById('dún').style.visibility = 'visible ';
